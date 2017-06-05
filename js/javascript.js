1fc0794a745ac5d411910aca70b93c2b35cec1c9
Jamer Helbert <jamer_tg_15@hotmail.com>
2017-06-05 12:59:47 -0500

    Version 2.4

--- js/javascript.js
+++ js/javascript.js
@@ -1,4 +1,15 @@
-	function changeImage() {
-		var pokeballImg = document.getElementById("icon01");
-		pokeballImg.src = "../img/master.png";
-	}
+function pageload() {
+	var ok = document.getElementById("icon01");
+	ok.onclick = okayClick;
+}
+
+function okayClick() {
+	alert("booyah");
+}
+
+window.onload = pageLoad;
+
+function changeImage() {
+	var pokeballImg = document.getElementById("icon01");
+	pokeballImg.src = "../img/master.png";
+}
