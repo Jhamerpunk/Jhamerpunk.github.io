c18bb828ecc01c34c7d5700473db3f8bb408ae21
Jamer Helbert <jamer_tg_15@hotmail.com>
2017-06-11 23:51:57 -0500

    Version 2.4

--- js/javascript.js
+++ js/javascript.js
@@ -12,28 +12,20 @@
 	}
 
 	
-	// Get the Sidebar
-	var mySidebar = document.getElementById("mySidebar");
-
-	// Get the DIV with overlay effect
-	var overlayBg = document.getElementById("myOverlay");
-
-	// Toggle between showing and hiding the sidebar, and add overlay effect
-	function w3_open() {
-	    if (mySidebar.style.display === 'block') {
-	        mySidebar.style.display = 'none';
-	        overlayBg.style.display = "none";
+	function w3_open(){
+	    if (document.getElementById("mySidebar").style.display === 'block') {
+	        document.getElementById("mySidebar").style.display = 'none';
+	        document.getElementById("myOverlay").style.display = "none";
 	    } else {
-	        mySidebar.style.display = 'block';
-	        overlayBg.style.display = "block";
+	        document.getElementById("mySidebar").style.display = 'block';
+	        document.getElementById("myOverlay").style.display = "block";
 	    }
 	}
 
-	// Close the sidebar with the close button
-	function w3_close() {
-	    mySidebar.style.display = "none";
-	    overlayBg.style.display = "none";
-	}
-	
+// Close the sidebar with the close button
+	function w3_close(){
+	    document.getElementById("mySidebar").style.display = "none";
+	    document.getElementById("myOverlay").style.display = "none";
+	};
 
 })();
